9069f9bcec53e3131d7255aaeb83f1d1d3c10803
NikolayValkov1994 <nvalkov@developsoft.com>
2024-01-10 19:19:57 +0200

    Upload my location app

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+	const [long, setLong] = useState();
+	const [lat, setLat] = useState();
+	const [error, setError] = useState({ message: null });
+
+	const loading = !long || !lat ? true : false;
+
+	const setCoordinates = (long, lat) => {
+		setLong(long);
+		setLat(lat);
+	};
+
+	const getLocationSuccessfully = (position) => {
+		const freshLong = position.coords.longitude.toFixed(7);
+		const freshLat = position.coords.latitude.toFixed(7);
+
+		if (freshLong !== long || freshLat !== lat) {
+			setCoordinates(freshLong, freshLat);
+		}
+	};
+
+	const getLocationIssue = (e) => {
+		setError(e);
+	};
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			navigator.geolocation.getCurrentPosition(
+				getLocationSuccessfully,
+				getLocationIssue
+			);
+		}, 1000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>
+				{error?.message
+					? error.message
+					: loading
+					? "Loading..."
+					: "long: " + long + " | " + "Lang: " + lat}
+			</h1>
+		</div>
+	);
+}
+
+export default App;
